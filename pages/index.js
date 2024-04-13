9796ce6d29d90bdd9f22b71e69f354adb23775aa
umer-i <79803255+umer-i@users.noreply.github.com>
2024-04-13 23:26:16 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Trader ( 20000 in funding) - Data scientist - drcglobal.org (100000 market cap) -   <code>umerbtc@protonmail.com</code>
         </p>
       </main>
 
